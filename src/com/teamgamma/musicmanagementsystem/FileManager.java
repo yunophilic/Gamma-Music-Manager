b9d56a8b9f89783ad330dfa4a4e7b4073508c359
Karen <cla233@sfu.ca>
2016-05-25 20:02:01 -0700

    Implement the generateSongs function in FileManager

--- src/com/teamgamma/musicmanagementsystem/FileManager.java
+++ src/com/teamgamma/musicmanagementsystem/FileManager.java
@@ -1,5 +1,7 @@
 package com.teamgamma.musicmanagementsystem;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,10 +15,41 @@ public class FileManager {
     }
 
     public List<Song> generateSongs(String pathToDirectory) {
-        List<Song> listOfSongs = new ArrayList<Song>();
+        List<Song> listOfSongs = new ArrayList<>();
 
+        File path = new File(pathToDirectory);
+        File[] musicFiles = getMusicFiles(path);
+
+        for (int i = 0; i < musicFiles.length; i++) {
+            listOfSongs.add(new Song(musicFiles[i].getAbsolutePath()));
+        }
+
+        // For debugging
+        /*for (int i = 0; i < listOfSongs.size(); i++) {
+            System.out.println("Song: " + listOfSongs.get(i).getM_songName());
+            System.out.println("      " + listOfSongs.get(i).getM_file().getAbsolutePath());
+        }*/
 
         return listOfSongs;
+    }
+
+    private File[] getMusicFiles(File path) {
+        return path.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                List<String> formats = new ArrayList<>();
+                formats.add(".mp3");
+
+                boolean isAccepted = false;
+                for (int i = 0; i < formats.size(); i++) {
+                    if (pathname.getName().endsWith(formats.get(i).toLowerCase())) {
+                        isAccepted = true;
+                        break;
+                    }
+                }
+                return isAccepted;
+            }
+        });
     }
 
 }
