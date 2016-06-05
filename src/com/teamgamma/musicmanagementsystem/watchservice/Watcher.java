3cf5941ea0664176ee632f9f204ad220496f8c88
Michael Trinh <mta81@sfu.ca>
2016-06-04 17:12:56 -0700

    Update WatchService to run on it's own thread

--- src/com/teamgamma/musicmanagementsystem/watchservice/Watcher.java
+++ src/com/teamgamma/musicmanagementsystem/watchservice/Watcher.java
@@ -1,5 +1,8 @@
 package com.teamgamma.musicmanagementsystem.watchservice;
 
+import com.teamgamma.musicmanagementsystem.Library;
+import com.teamgamma.musicmanagementsystem.SongManager;
+import javafx.application.Platform;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -7,29 +10,50 @@ import java.nio.file.attribute.BasicFileAttributes;
 public class Watcher {
     private WatchService m_watcher;
     private WatchKey m_watchKey;
+    private Thread m_watcherThread;
+    private SongManager model;
 
-    public Watcher() {
+    public Watcher(SongManager model) {
         try {
+            this.model = model;
             m_watcher = FileSystems.getDefault().newWatchService();
+
+            for (Library lib : model.getM_libraries()) {
+                addWatchDir(lib.getM_rootDirPath());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void startWatcher() {
-        try {
-            while(true) {
-                m_watchKey = m_watcher.take();
-                for (WatchEvent<?> event : m_watchKey.pollEvents()) {
-                    WatchEvent.Kind<?> kind = event.kind();
-                    Path eventPath = (Path) event.context();
-                    System.out.println(kind + ": " + eventPath);
-                    m_watchKey.reset();
+        m_watcherThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("**** Watching...");
+
+                try {
+                    while(true) {
+                        m_watchKey = m_watcher.take();
+
+                        for (WatchEvent<?> event : m_watchKey.pollEvents()) {
+                            WatchEvent.Kind<?> kind = event.kind();
+                            Path eventPath = (Path) event.context();
+                            System.out.println(kind + ": " + eventPath);
+                            m_watchKey.reset();
+                            Platform.runLater(() -> {
+                                model.notifyFileObservers();
+                            });
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("SOMTHING WENT WRONG HERE!");
+                    e.printStackTrace();
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        });
+
+        m_watcherThread.start();
     }
 
     public void addWatchDir(String rootDir) {
@@ -50,14 +74,6 @@ public class Watcher {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public WatchService getWatcher() {
-        return m_watcher;
-    }
-
-    public WatchKey getWatchKey() {
-        return m_watchKey;
     }
 
 }
