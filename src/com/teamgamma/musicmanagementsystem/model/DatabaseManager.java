c856940a1bfd602db5230186d563de37acc4700f
kwa52 <kwa52@sfu.ca>
2016-06-14 19:56:53 -0700

    Implement connecting to a database fix #42

--- src/com/teamgamma/musicmanagementsystem/model/DatabaseManager.java
+++ src/com/teamgamma/musicmanagementsystem/model/DatabaseManager.java
@@ -1,8 +1,11 @@
 package com.teamgamma.musicmanagementsystem.model;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 
 /**
@@ -11,14 +14,23 @@ import java.sql.SQLException;
 public class DatabaseManager {
 
     private Connection connection = null;
+    private Statement stmt;
+    private ResultSet rs;
 
     public DatabaseManager() {
+        connectToDatabase();
+    }
 
+    /**
+     * Connect to a database, a database file will be created if there is no database object yet
+     */
+    private void connectToDatabase(){
         try {
             Class.forName("org.sqlite.JDBC");
             System.out.println("Connecting to database...");
             // establish connection
-            connection = DriverManager.getConnection("jdbc:sqlite:dbFile.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.dir") +
+                    File.separator + "db" + File.separator + "dbFile.db");
 
         }
         catch(ClassNotFoundException e) {
@@ -27,6 +39,5 @@ public class DatabaseManager {
         catch(SQLException e) {
             e.printStackTrace();
         }
-
     }
 }
