6273a61009a41bd3d0e1bf320c557c43f769109a
Ivan Jonathan Hoo <ihoo@sfu.ca>
2016-06-18 18:30:25 -0700

    fix issue #61

--- src/com/teamgamma/musicmanagementsystem/ApplicationController.java
+++ src/com/teamgamma/musicmanagementsystem/ApplicationController.java
@@ -22,6 +22,8 @@ public class ApplicationController extends Application {
     private static final double MIN_WINDOW_WIDTH = 800;
     private static final double MIN_WINDOW_HEIGHT = 400;
 
+    private DatabaseManager m_databaseManager;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -31,24 +33,27 @@ public class ApplicationController extends Application {
         primaryStage.setTitle("Music Management System");
 
         SongManager songManager = new SongManager();
-        DatabaseManager databaseManager = new DatabaseManager();
-        if (!databaseManager.isThereSavedState()) {
+        m_databaseManager = new DatabaseManager();
+        if (!m_databaseManager.isThereSavedState()) {
             System.out.println("No libraries are existent");
             System.out.println("creating new library storage file...");
-            databaseManager.createFileLibraries();
+            m_databaseManager.createDatabaseFile();
             String firstLibrary = PromptUI.initialWelcome();
             songManager.addLibrary(firstLibrary);
-            databaseManager.addLibrary(firstLibrary);
+            m_databaseManager.setupDatabase();
+            m_databaseManager.addLibrary(firstLibrary);
+        } else {
+            m_databaseManager.setupDatabase();
         }
-        List<String> librariesPath = databaseManager.getLibraries();
+        List<String> librariesPath = m_databaseManager.getLibraries();
         System.out.println("loading libraries...");
         for (String libPath : librariesPath) {
             songManager.addLibrary(libPath);
         }
 
         MusicPlayerManager musicPlayerManager = new MusicPlayerManager();
-        MainUI rootUI = new MainUI(songManager, musicPlayerManager, databaseManager);
-        Watcher watcher = new Watcher(songManager, databaseManager);
+        MainUI rootUI = new MainUI(songManager, musicPlayerManager, m_databaseManager);
+        Watcher watcher = new Watcher(songManager, m_databaseManager);
         watcher.startWatcher();
 
         primaryStage.setOnCloseRequest(e -> {
@@ -62,11 +67,10 @@ public class ApplicationController extends Application {
         primaryStage.setMinHeight(MIN_WINDOW_HEIGHT);
         primaryStage.setMinWidth(MIN_WINDOW_WIDTH);
         primaryStage.show();
-
     }
 
     @Override
     public void stop() {
-
+        m_databaseManager.closeConnection();
     }
 }
--- src/com/teamgamma/musicmanagementsystem/model/DatabaseManager.java
+++ src/com/teamgamma/musicmanagementsystem/model/DatabaseManager.java
@@ -6,7 +6,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -17,34 +16,14 @@ import java.util.List;
  * Class to manage database
  */
 public class DatabaseManager {
-    private Connection connection;
-    private Statement statement;
-    private ResultSet resultSet;
+    private static final String DB_DIR = System.getProperty("user.dir") + File.separator + "db";
+    private static final String DB_FILE_PATH = DB_DIR + File.separator + "persistence.db";
+
+    private Connection m_connection;
+    private Statement m_statement;
+    private ResultSet m_resultSet;
 
     public DatabaseManager() {
-        connectToDatabase();
-    }
-
-    /**
-     * Connect to a database, a database file will be created if there is no database object yet
-     */
-    private void connectToDatabase(){
-        try {
-            Class.forName("org.sqlite.JDBC");
-            System.out.println("Connecting to database...");
-            new File(System.getProperty("user.dir") + File.separator + "db").mkdir();
-
-            // establish connection
-            connection = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.dir") +
-                    File.separator + "db" + File.separator + "dbFile.db");
-
-        }
-        catch(ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        catch(SQLException e) {
-            e.printStackTrace();
-        }
     }
 
     /**
@@ -53,43 +32,126 @@ public class DatabaseManager {
      * @return true if something, false otherwise
      */
     public boolean isThereSavedState() {
-        return new File(System.getProperty("user.dir") + File.separator + "db" +
-                File.separator + "libraries.txt").exists();
+        return new File(DB_FILE_PATH).exists();
     }
 
     /**
      * Function to create .txt file to save library names
-     *
-     * @return true if something is saved
      */
-    public boolean createFileLibraries() {
-        Path libPath = Paths.get(System.getProperty("user.dir") + File.separator + "db" +
-                File.separator + "libraries.txt");
+    public void createDatabaseFile() {
+        Path dbFilePath = Paths.get(DB_FILE_PATH);
         try {
-            Files.createDirectories(libPath.getParent());
-            Files.createFile(libPath);
+            Files.createDirectories(dbFilePath.getParent());
+            Files.createFile(dbFilePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        return isThereSavedState();
+    /**
+     * Initialize database
+     */
+    public void setupDatabase() {
+        setupConnection();
+        createTables();
+    }
+
+    /**
+     * Init database connection
+     */
+    private void setupConnection(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            System.out.println("Connecting to database...");
+            m_connection = DriverManager.getConnection("jdbc:sqlite:" + DB_FILE_PATH);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Close connection
+     */
+    public void closeConnection() {
+        try {
+            System.out.println("Closing connection...");
+            m_connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Create tables if not exist
+     */
+    private void createTables() {
+        try {
+            m_statement = m_connection.createStatement();
+
+            //library table
+            m_statement.executeUpdate("CREATE TABLE IF NOT EXISTS Library (" +
+                                            "libraryPath TEXT PRIMARY KEY NOT NULL" +
+                                      ")");
+
+            //left tree view table
+            m_statement.executeUpdate("CREATE TABLE IF NOT EXISTS LeftTreeView (" +
+                                            "path TEXT PRIMARY KEY NOT NULL," +
+                                            "expanded BOOLEAN NOT NULL," +
+                                            "selected BOOLEAN NOT NULL" +
+                                      ")");
+
+            //right tree view table
+            m_statement.executeUpdate("CREATE TABLE IF NOT EXISTS LeftTreeView (" +
+                                            "path TEXT PRIMARY KEY NOT NULL," +
+                                            "expanded BOOLEAN NOT NULL" +
+                                      ")");
+
+            m_statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Updates file by adding a new library
      *
-     * @param libraryToSave
+     * @param libraryPath String
      */
-    public void addLibrary(String libraryToSave) {
-        File findLibFile = new File(System.getProperty("user.dir") + File.separator + "db" +
+    public void addLibrary(String libraryPath) {
+        /*File dbFile = new File(System.getProperty("user.dir") + File.separator + "db" +
                 File.separator + "libraries.txt");
         try {
-            PrintWriter writer = new PrintWriter(new FileWriter(findLibFile, true));
-            writer.println(libraryToSave);
+            PrintWriter writer = new PrintWriter(new FileWriter(dbFile, true));
+            writer.println(libraryPath);
             writer.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        try {
+            m_statement = m_connection.createStatement();
+            m_statement.executeUpdate("INSERT INTO Library VALUES ('" + libraryPath + "')");
+            m_statement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Updates file by adding multiple libraries
+     *
+     * @param librariesPath List<String>
+     */
+    public void addLibraries(List<String> librariesPath) {
+        try {
+            m_statement = m_connection.createStatement();
+            for(String libraryPath : librariesPath) {
+                m_statement.executeUpdate("INSERT INTO Library VALUES ('" + libraryPath + "')");
+            }
+            m_statement.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -97,39 +159,39 @@ public class DatabaseManager {
     /**
      * Updates file by removing an existing library
      *
-     * @param libraryToRemove
-     * @return true if storage update successful
+     * @param libraryPath String
+     * @return success or fail boolean
      */
-    public boolean removeLibrary(String libraryToRemove) {
-        File findLibFile = new File(System.getProperty("user.dir") + File.separator + "db" +
-                File.separator + "libraries.txt");
-        File tempFile = new File(System.getProperty("user.dir") + File.separator + "db" +
-                File.separator + "temp.txt");
-
-        boolean success = false;
+    public boolean removeLibrary(String libraryPath) {
         try {
-            BufferedReader buffer = new BufferedReader(new FileReader(findLibFile));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-
-            String line;
-            while ((line = buffer.readLine()) != null) {
-                String trimmedLine = line.trim();
-                if (trimmedLine.equals(libraryToRemove)) {
-                    continue;
-                }
-                writer.write(line + System.getProperty("line.separator"));
-            }
-            writer.close();
-            buffer.close();
-            findLibFile.delete();
-            success = tempFile.renameTo(new File(findLibFile.getAbsolutePath()));
-        } catch (FileNotFoundException e) {
+            m_statement = m_connection.createStatement();
+            m_statement.executeUpdate("DELETE FROM Library WHERE libraryPath='" + libraryPath + "'");
+            m_statement.close();
+            return true;
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            return false;
         }
+    }
 
-        return success;
+    /**
+     * Updates file by removing multiple existing libraries
+     *
+     * @param librariesPath List<String>
+     * @return success or fail boolean
+     */
+    public boolean removeLibraries(List<String> librariesPath) {
+        try {
+            m_statement = m_connection.createStatement();
+            for (String libraryPath : librariesPath) {
+                m_statement.executeUpdate("DELETE FROM Library WHERE libraryPath='" + libraryPath + "'");
+            }
+            m_statement.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     /**
@@ -138,21 +200,19 @@ public class DatabaseManager {
      * @return an ArrayList of libraries path
      */
     public List<String> getLibraries() {
-        File findLibFile = new File(System.getProperty("user.dir") + File.separator + "db" +
-                File.separator + "libraries.txt");
         List<String> librariesPath = new ArrayList<>();
         try {
-            BufferedReader buffer = new BufferedReader(new FileReader(findLibFile));
-            String line;
-            while ((line = buffer.readLine()) != null) {
-                librariesPath.add(line);
+            m_statement = m_connection.createStatement();
+            m_resultSet = m_statement.executeQuery("SELECT * FROM Library");
+            while(m_resultSet.next()) {
+                librariesPath.add(m_resultSet.getString("libraryPath"));
             }
-            buffer.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            m_resultSet.close();
+            m_statement.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
         return librariesPath;
     }
 }
