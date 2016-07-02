7f2738acee939b5b4572af4e53ab249e0333dfd4
Ivan Jonathan Hoo <ihoo@sfu.ca>
2016-07-02 05:11:34 -0700

    fix issue #117

--- src/com/teamgamma/musicmanagementsystem/misc/ContextMenuConstants.java
+++ src/com/teamgamma/musicmanagementsystem/misc/ContextMenuConstants.java
@@ -12,8 +12,9 @@ public class ContextMenuConstants {
     public static final String RENAME_THIS_FILE = "Rename";
     public static final String EDIT_PROPERTIES = "Edit Properties";
     public static final String ADD_TO_PLAYLIST = "Add to Playlist";
-    public static final String CREATE_NEW_PLAYLIST = "Create New Playlist";
+    /*public static final String CREATE_NEW_PLAYLIST = "Create New Playlist";*/
     public static final String MENU_ITEM_PLAY_SONG = "Play Song";
     public static final String MENU_ITEM_PLAY_SONG_NEXT = "Play Song Next";
     public static final String MENU_ITEM_PLACE_SONG_ON_QUEUE = "Place Song On Queue";
+    public static final String REMOVE_FROM_PLAYLIST = "Remove From Playlist";
 }
