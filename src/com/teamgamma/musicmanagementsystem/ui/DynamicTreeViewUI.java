92487013d6a65b1a0b1e160ee0d04583bf472f93
Ivan Jonathan Hoo <ihoo@sfu.ca>
2016-07-17 22:57:50 -0700

    remove commented out code

--- src/com/teamgamma/musicmanagementsystem/ui/DynamicTreeViewUI.java
+++ src/com/teamgamma/musicmanagementsystem/ui/DynamicTreeViewUI.java
@@ -109,7 +109,6 @@ public class DynamicTreeViewUI extends StackPane {
      */
     private TreeView<Item> createTrees(List<Library> libraries, List<String> dynamicTreeViewExpandedPaths) {
         if (!libraries.isEmpty()) {
-            //File dummyRootFile = new File(libraries.get(0).getRootDirPath());
             TreeItem<Item> root = new TreeItem<>(new DummyItem());
 
             TreeItem<Item> rootItem = FileTreeUtils.copyTree(m_model.search(m_model.getM_rightFolderSelected()));
