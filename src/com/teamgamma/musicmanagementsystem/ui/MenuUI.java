90ec37de9257ae6c4bcb72398ab8c5fc78209cdf
Michael Trinh <mta81@sfu.ca>
2016-07-23 16:56:13 -0700

    Refactor code in MenuUI class

--- src/com/teamgamma/musicmanagementsystem/ui/MenuUI.java
+++ src/com/teamgamma/musicmanagementsystem/ui/MenuUI.java
@@ -16,6 +16,22 @@ import java.io.File;
  * Class for the Menu Bar
  */
 public class MenuUI extends MenuBar{
+    private static final String FILE_TITLE = "File";
+    private static final String ADD_LIBRARY_OPTION = "Add Library";
+    private static final String OPTIONS_TITLE = "Options";
+    private static final String LEFT_PANEL_OPTION = "Left Panel";
+    private static final String SHOW_FOLDERS_ONLY = "Show folders only";
+    private static final String DISPLAY_FOLDERS_ONLY = "Display folders only";
+    private static final String DONT_DISPLAY_FOLDERS_ONLY = "Don't display folders only";
+    private static final String CENTER_PANEL_OPTION = "Center Panel";
+    private static final String SHOW_FILES_IN_SUBFOLDERS = "Show files in subfolders";
+    private static final String DISPLAY_SUBFOLDER_FILES = "Display subfolder files";
+    private static final String DONT_DISPLAY_SUBFOLDER_FILES = "Don't display subfolder files";
+    private static final String PLAYLIST_TITLE = "Playlist";
+    private static final String CREATE_NEW_PLAYLIST = "Create New Playlist";
+    private static final String REMOVE_EXISTING_PLAYLIST = "Remove Existing Playlist";
+    private static final String EXPORT_PLAYLIST = "Export Playlist";
+
     private SongManager m_model;
     private DatabaseManager m_databaseManager;
 
@@ -44,8 +60,8 @@ public class MenuUI extends MenuBar{
     }
 
     private Menu getMenuFile() {
-        final Menu menuFile = new Menu("File");
-        MenuItem addLibraryMenu = new MenuItem("Add Library");
+        final Menu menuFile = new Menu(FILE_TITLE);
+        MenuItem addLibraryMenu = new MenuItem(ADD_LIBRARY_OPTION);
         addLibraryMenu.setOnAction(event -> {
             String pathInput = PromptUI.addNewLibrary();
             if (pathInput == null) {
@@ -71,7 +87,7 @@ public class MenuUI extends MenuBar{
      * @return The menu options
      */
     private Menu getMenuOptions(FilePersistentStorage filePersistentStorage) {
-        final Menu menuOptions = new Menu("Options");
+        final Menu menuOptions = new Menu(OPTIONS_TITLE);
         Menu leftPanelSubMenu = getLeftPanelSubMenu(filePersistentStorage);
         Menu centerPanelSubMenu = getCenterPanelSubMenu(filePersistentStorage);
 
@@ -86,17 +102,17 @@ public class MenuUI extends MenuBar{
      * @return The left panel sub menu
      */
     private Menu getLeftPanelSubMenu(FilePersistentStorage config) {
-        Menu leftPanelSubMenu = new Menu("Left Panel");
-        CheckMenuItem showFoldersOnly = new CheckMenuItem("Show folders only");
+        Menu leftPanelSubMenu = new Menu(LEFT_PANEL_OPTION);
+        CheckMenuItem showFoldersOnly = new CheckMenuItem(SHOW_FOLDERS_ONLY);
         showFoldersOnly.setSelected(config.getLeftPanelOption());
 
         showFoldersOnly.setOnAction(event -> {
             if (showFoldersOnly.isSelected()){
-                System.out.println("Display folders only");
+                System.out.println(DISPLAY_FOLDERS_ONLY);
                 MenuOptions menuManager = m_model.getM_menuOptions();
                 menuManager.setM_leftPanelShowFoldersOnly(true);
             } else {
-                System.out.println("Don't display folders only");
+                System.out.println(DONT_DISPLAY_FOLDERS_ONLY);
                 MenuOptions menuManager = m_model.getM_menuOptions();
                 menuManager.setM_leftPanelShowFoldersOnly(false);
             }
@@ -115,17 +131,17 @@ public class MenuUI extends MenuBar{
      * @return The center panel sub menu
      */
     private Menu getCenterPanelSubMenu(FilePersistentStorage config) {
-        Menu centerPanelSubMenu = new Menu("Center Panel");
-        CheckMenuItem showFoldersOnly = new CheckMenuItem("Show files in subfolders");
+        Menu centerPanelSubMenu = new Menu(CENTER_PANEL_OPTION);
+        CheckMenuItem showFoldersOnly = new CheckMenuItem(SHOW_FILES_IN_SUBFOLDERS);
         showFoldersOnly.setSelected(config.getCenterPanelOption());
 
         showFoldersOnly.setOnAction(event -> {
             if (showFoldersOnly.isSelected()){
-                System.out.println("Display subfolder files");
+                System.out.println(DISPLAY_SUBFOLDER_FILES);
                 MenuOptions menuManager = m_model.getM_menuOptions();
                 menuManager.setM_centerPanelShowSubfolderFiles(true);
             } else {
-                System.out.println("Don't subfolder files");
+                System.out.println(DONT_DISPLAY_SUBFOLDER_FILES);
                 MenuOptions menuManager = m_model.getM_menuOptions();
                 menuManager.setM_centerPanelShowSubfolderFiles(false);
             }
@@ -143,9 +159,9 @@ public class MenuUI extends MenuBar{
      * @return The playlist sub menu
      */
     private Menu getPlaylistSubMenu() {
-        Menu playlistSubMenu = new Menu("Playlist");
+        Menu playlistSubMenu = new Menu(PLAYLIST_TITLE);
 
-        MenuItem createNewPlaylistMenu = new MenuItem("Create New Playlist");
+        MenuItem createNewPlaylistMenu = new MenuItem(CREATE_NEW_PLAYLIST);
         createNewPlaylistMenu.setOnAction(event -> {
             String newPlaylistName = PromptUI.createNewPlaylist();
             if (m_model.playlistNameExist(newPlaylistName)) {
@@ -159,7 +175,7 @@ public class MenuUI extends MenuBar{
             }
         });
 
-        MenuItem removePlaylistMenu = new MenuItem("Remove Existing Playlist");
+        MenuItem removePlaylistMenu = new MenuItem(REMOVE_EXISTING_PLAYLIST);
         removePlaylistMenu.setOnAction(event -> {
             Playlist playlistToRemove = PromptUI.removePlaylistSelection(m_model.getM_playlists());
             if (playlistToRemove != null) {
@@ -169,7 +185,7 @@ public class MenuUI extends MenuBar{
             }
         });
 
-        MenuItem exportPlaylistMenu = new MenuItem("Export Playlist");
+        MenuItem exportPlaylistMenu = new MenuItem(EXPORT_PLAYLIST);
         exportPlaylistMenu.setOnAction(event -> {
             Pair<Playlist, File> playlistFilePair = PromptUI.exportPlaylist(m_model.getM_playlists());
             if (playlistFilePair != null) {
