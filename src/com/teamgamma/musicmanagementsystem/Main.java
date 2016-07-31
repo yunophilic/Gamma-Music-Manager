d909462ea536623c33205bc621cbdf724079e55a
Karen <cla233@sfu.ca>
2016-07-30 22:34:29 -0700

    Extract port number as constant

--- src/com/teamgamma/musicmanagementsystem/Main.java
+++ src/com/teamgamma/musicmanagementsystem/Main.java
@@ -10,12 +10,15 @@ import java.net.ServerSocket;
  * Taken from https://blog.codecentric.de/en/2015/09/javafx-how-to-easily-implement-application-preloader-2/
  */
 public class Main {
+
+    // According to https://en.wikipedia.org/wiki/Ephemeral_port, ports 49152 to 65535 are suggested
+    private static final int PORT_NUMBER = 49152;
+
     public static void main(String[] args) {
         // Launch application if the server socket is not already open (i.e. no other instances running)
         // otherwise, exit the app
         try {
-            // According to https://en.wikipedia.org/wiki/Ephemeral_port, ports 49152 to 65535 are suggested
-            ServerSocket serverSocket = new ServerSocket(49152);
+            ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
             LauncherImpl.launchApplication(ApplicationController.class, StartUpLoader.class, args);
         } catch (IOException x) {
             System.out.println("Another instance already running... exit.");
